c3ef5393a827a3a26952c2633b8a7843df48fafa
依然夏末 <54070326+yiranxiamo@users.noreply.github.com>
2022-01-12 15:13:40 +0800

    Update foot.php

--- foot.php
+++ foot.php
@@ -1,11 +1,11 @@
 </script>
-<script src='https://cdn.jsdelivr.net/gh/HCLonely/Valine@latest/dist/Valine.min.js'></script>
+<script src='//unpkg.com/valine/dist/Valine.min.js'></script>
 <div id="vcomments"></div>
 <script>
     new Valine({
         el: '#vcomments',
-        appId: 'i5Vv8Tf5tv4GTUsXil6mtzlM-gzGzoHsz',
-        appKey: 'Sqai6FV8ffhb0YHuqDcd7bo7',
+        appId: '填入你自己的id',
+        appKey: '填入你自己的key ',
         avatar:'wavatar',
         placeholder:'发表评论，填入QQ邮箱显示头像噢',
         lang:'zh-cn'
